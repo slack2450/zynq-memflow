22faa99a945a7c78525aca9882c9fdccd432fc77
Joss Bird <joss@localhost.localdomain>
2023-02-25 17:38:27 +0000

    update code to memflow

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
-use std::fs::File;
-use memflow::{prelude::v1::{Result}, MemoryMap, FileIOMemory};
-use memflow_win32::Kernel;
+use cloneable_file::CloneableFile;
+use memflow::{prelude::{v1::{Result}}};
+use memflow::mem::MemoryMap;
+use memflow::connector::FileIoMemory;
+use memflow_win32::win32::Win32Kernel;
 
 use simplelog::{LevelFilter, TermLogger, Config, TerminalMode };
 
+
 fn main() -> Result<()> {
     TermLogger::init(LevelFilter::Trace, Config::default(), TerminalMode::Mixed, simplelog::ColorChoice::Auto).unwrap();
-
-    let file = File::open("/dev/ax_dma_0").unwrap();
+    
+    let file = CloneableFile::open("/dev/ax_dma_0").unwrap();
 
     let mut map = MemoryMap::new();
 
     map.push_remap(0x0.into(), !0, 0x0.into());
-    let connector = FileIOMemory::try_with_reader(file, map)?;
+    let connector = FileIoMemory::try_with_reader(file, map)?;
 
-    let kernel = Kernel::builder(connector).build().unwrap();
+    let kernel = Win32Kernel::builder(connector).no_symbol_store().build().unwrap();
 
     println!("{:?}", kernel);
 
